34eb5dee1f1f22796bf140e4e34b1123239ae575
Nezih <20neziht@gmail.com>
2017-02-16 21:49:19 +0300

    Ticket ile ilgili hersey tek bir controller icine alindi.

--- application/controllers/Ticket.php
+++ application/controllers/Ticket.php
@@ -1,20 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TicketAc extends CI_Controller {
+class Ticket extends CI_Controller {
 
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('ticketgonder_model');
-        $ticketSuccess = isset($this->session->userdata["ticketSuccess"]) ? $this->session->userdata["ticketSuccess"] : 2;
-        $this->twig->addGlobal('ticketSuccess', $ticketSuccess);
     }
     public function index()
     {
+        
+    }
+    public function open(){
+        //Open Ticket
+        $ticketSuccess = isset($this->session->userdata["ticketSuccess"]) ? $this->session->userdata["ticketSuccess"] : 2;
+        $this->twig->addGlobal('ticketSuccess', $ticketSuccess);
         $this->twig->display('pages/ticketac');
     }
-    public function gonder(){
+    public function create(){
         $args = [
             "ticket_from" => $this->session->user_session["user_id"],
             "ticket_to" => $this->input->post('ticket_to'),
@@ -22,6 +25,19 @@ class TicketAc extends CI_Controller {
             "ticket_content" => $this->input->post('ticket_content'),
             "ticket_priority" => $this->input->post('ticket_priority')
         ];
+        $this->load->model('ticketgonder_model');
         $this->ticketgonder_model->gonder($args);
+    }
+    public function show($ticket_id){
+        //Show Ticket
+    }
+    public function edit($ticket_id){
+        //Edit Ticket
+    }
+    public function delete($ticket_id){
+        //Delete Ticket
+    }
+    public function all(){
+        $this->twig->display('pages/acikticketlar');
     }
 }
